f4b2686e0c952d9e01d00574d4f84eca88d4877a
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-02-28 09:25:00 +0000

    Import alumno

---
+++ main.py
@@ -0,0 +1,18 @@
+import alumno
+
+def main():
+    pass
+
+def main():
+    javier = alumno.alumno()
+    javier.nota = 6
+    javier.imprimir()
+    javier.promociona()
+
+    diego = alumno.alumno()
+    diego.nota = 3
+    diego.imprimir()
+    diego.promociona()
+
+if __name__ == "__main__":
+    main()
