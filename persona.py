33f2ee783f3dcfbf413342f4768832b1faa11b6e
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-02-28 12:23:08 +0000

    Def imprimir

--- persona.py
+++ persona.py
@@ -2,3 +2,6 @@ class persona:
     def __init__(self, nombre, anos):
         self.nombre = nombre
         self.anos = anos
+
+    def imprimir(self):
+        print (self.nombre,"cumplió años, ahora tiene:", self.nota)
