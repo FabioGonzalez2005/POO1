b7b932559c752186244c5144f137fcbb00d596df
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-02-28 09:30:38 +0000

    Añadido constructor

--- alumno.py
+++ alumno.py
@@ -1,20 +1,16 @@
 class alumno:
-    nota = 0
+    def __init__(self):
+        self.nota = 0
+        self.nombre = ""
 
     def imprimir(self):
+        print (self.nombre,"obtiene:", self.nota)
+
+    def promociona(self):
         if self.nota >= 5:
             print("Promociona.")
         else:
             print("No promociona.")
-
-def main():
-    javier = alumno()
-    javier.nota = 6
-    javier.imprimir()
-
-    diego = alumno()
-    diego.nota = 3
-    diego.imprimir()
 
 if __name__ == "__main__":
     main()
